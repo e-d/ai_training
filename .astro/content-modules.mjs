fc57ed8a9b530d974156e7f102c6e0949e9a03fb
Ed St. Louis <ed.st.louis@prioritydispatch.net>
2026-04-27 18:29:21 -0600

    feat: Add 8 placeholder lesson MDX files for Course 1 and dynamic cours…
    - src/content/lessons/lesson-1-1.mdx
    - src/content/lessons/lesson-1-2.mdx
    - src/content/lessons/lesson-1-3.mdx
    - src/content/lessons/lesson-1-4.mdx
    - src/content/lessons/lesson-1-5.mdx
    - src/content/lessons/lesson-1-6.mdx
    - src/content/lessons/lesson-1-7.mdx
    - src/content/lessons/lesson-1-8.mdx
    
    GSD-Task: S02/T01

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,11 @@
-export default new Map();
+
+export default new Map([
+["src/content/lessons/lesson-1-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-1.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-2.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-3.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-4.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-5.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-6.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-6.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-7.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-7.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-1-8.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-8.mdx&astroContentModuleFlag=true")]]);
+		
