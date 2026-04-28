700fca24bb9aa554747f9443fd415837e0375787
Ed St. Louis <ed.st.louis@prioritydispatch.net>
2026-04-27 23:44:16 -0600

    gsd snapshot: pre-dispatch, uncommitted changes after 215m inactivity

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -7,5 +7,11 @@ export default new Map([
 ["src/content/lessons/lesson-1-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-5.mdx&astroContentModuleFlag=true")],
 ["src/content/lessons/lesson-1-6.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-6.mdx&astroContentModuleFlag=true")],
 ["src/content/lessons/lesson-1-7.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-7.mdx&astroContentModuleFlag=true")],
-["src/content/lessons/lesson-1-8.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-8.mdx&astroContentModuleFlag=true")]]);
+["src/content/lessons/lesson-1-8.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-1-8.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-2-1.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-2-1.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-2-2.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-2-2.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-2-3.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-2-3.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-2-4.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-2-4.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-2-5.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-2-5.mdx&astroContentModuleFlag=true")],
+["src/content/lessons/lesson-2-6.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2Flesson-2-6.mdx&astroContentModuleFlag=true")]]);
 		
